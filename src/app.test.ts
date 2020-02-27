f4168593129f19dc0d236a3fc84f4b76d3f911d9
manuel birba <manuel.birba-sc@alehos.fr>
2020-02-27 11:18:13 +0100

    testing api without warnings

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,48 +1,31 @@
 import request from "supertest"
-import app  from "./app"
-import { doesNotMatch } from "assert";
-// let server = request(app);
+import {app, server}  from "./app"
 
-function closePromises(pm:any){
-  console.log(pm)
-};
-
-// afterAll((done) => {
-//   // closePromises(done);
-
-//   //server.close(done);
-// });
 
 describe("api is ready ('/')",()=>{
 
-  test('status is ok', () => {
-     request(app)
+  test('status is ok', async (done) => {
+    await request(app)
         .get('/')
         .expect(200)
-        .end(function(err, res) {
-          if (err) throw err;
-        });
+        done()
   });
 
-  test('Content-Type is ok', () => {
-      request(app)
+  test('Content-Type is ok', async (done) => {
+      await request(app)
         .get('/')
         .expect('Content-Type', /json/)
-        .end(function(err, res) {
-          if (err) throw err;
-        });
+        done()
   });
 });
 
 describe("response object",()=>{
-  test('response object got success property is ok', async (done) => {
-  await request(app)
-  .get('/')
-  .expect('Content-Type', /json/)
-  .expect(200)
-  // .end(function(err, res) {
-  //   if (err) throw err;
-    done()
-  // });
-  })
+
+    test('response object got success property is ok', async (done) => {
+      await request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      done()
+    })
 });
--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,14 @@ const app = express()
 const port = 8888
 
 function home(req : any, res : any){
-	res.status(200).json({succes:`Welcome api running at port:${port}`})
+    res.status(200).json({succes:`Welcome api running at port:${port}`})
 }
 app.get("/", home)
 
 var server = app.listen(port, ()=>{
-	if (process.env.NODE_ENV === 'test') {
-			server.close();
-		setTimeout(function () {
-            console.log("close in 3 secondes")
-		console.log("test server listening on port " + port)
-		}, 1000)
-	}
+    if (process.env.NODE_ENV === 'test') {
+            server.close();
+    }
 })
 
-export default app
+export {app, server}
